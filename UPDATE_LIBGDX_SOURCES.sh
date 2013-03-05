8f6d774bca21482f486ad1e10326b52ed61cda79
Andrei Lisnic <andrei.lisnic@gmail.com>
2013-03-05 16:37:21 +0200

    Remove useless copy and rm lines, since wget creates the folders correctly

--- UPDATE_LIBGDX_SOURCES.sh
+++ UPDATE_LIBGDX_SOURCES.sh
@@ -1,15 +1,7 @@
 set -e
 
-echo "\nDownloading box2d from libgdx..."
+echo "\nDownloading libgdx box2d native headers..."
 wget -r -k --tries 10 --reject=html -np -P jni/Box2D http://libgdx.googlecode.com/svn/trunk/gdx/jni/Box2D/
-echo "\nCopying files..."
-cp jni/Box2D/libgdx.googlecode.com/svn/trunk/gdx/jni/Box2D/**.* jni/Box2D
-echo "Removing junk..."
-rm -r jni/Box2D/libgdx.googlecode.com
 
-echo "\nDownloading box2d physics from libgdx..."
+echo "\nDownloading libgdx box2d java libs..."
 wget -r -k --tries 10 --reject=html -np -P src http://libgdx.googlecode.com/svn/trunk/gdx/src/com/badlogic/gdx/physics/box2d/
-echo "\nCopying files..."
-cp src/libgdx.googlecode.com/svn/trunk/gdx/src/com/badlogic/gdx/physics/**.* src/com/badlogic/gdx/physics
-echo "\nRemoving junk..."
-rm -r src/libgdx.googlecode.com/
