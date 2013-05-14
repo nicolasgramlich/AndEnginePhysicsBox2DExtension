2e3dd97f33944eeec1101ada61a0bf1af36bbb26
Michal Stawinski <michal.stawinski@sonymobile.com>
2013-05-14 18:39:01 +0200

    PhysicsConnectorManager should be able to work with IEntities too- not IShape only

--- src/org/andengine/extension/physics/box2d/PhysicsConnectorManager.java
+++ src/org/andengine/extension/physics/box2d/PhysicsConnectorManager.java
@@ -3,7 +3,7 @@ package org.andengine.extension.physics.box2d;
 import java.util.ArrayList;
 
 import org.andengine.engine.handler.IUpdateHandler;
-import org.andengine.entity.shape.IShape;
+import org.andengine.entity.IEntity;
 
 import com.badlogic.gdx.physics.box2d.Body;
 
@@ -61,7 +61,7 @@ public class PhysicsConnectorManager extends ArrayList<PhysicsConnector> impleme
 	// Methods
 	// ===========================================================
 
-	public Body findBodyByShape(final IShape pShape) {
+	public Body findBodyByShape(final IEntity pShape) {
 		final ArrayList<PhysicsConnector> physicsConnectors = this;
 		for (int i = physicsConnectors.size() - 1; i >= 0; i--) {
 			final PhysicsConnector physicsConnector = physicsConnectors.get(i);
@@ -72,7 +72,7 @@ public class PhysicsConnectorManager extends ArrayList<PhysicsConnector> impleme
 		return null;
 	}
 
-	public PhysicsConnector findPhysicsConnectorByShape(final IShape pShape) {
+	public PhysicsConnector findPhysicsConnectorByShape(final IEntity pShape) {
 		final ArrayList<PhysicsConnector> physicsConnectors = this;
 		for (int i = physicsConnectors.size() - 1; i >= 0; i--) {
 			final PhysicsConnector physicsConnector = physicsConnectors.get(i);
